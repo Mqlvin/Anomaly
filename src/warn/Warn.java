d64af27242700ea41244c97a289675dbc17d114b
Mqlvin <pricklydotcactus@gmail.com>
2021-09-13 08:11:41 +0100

    Warning class updated.

--- src/warn/Warn.java
+++ src/warn/Warn.java
@@ -1,11 +1,11 @@
 package warn;
 
-import com.google.gson.JsonObject;
 import mail.Sender;
+import profile.SchedulerProfile;
 
 public class Warn {
-    public static void sendWarning(String uuid, JsonObject settings) {
-        if(settings.get("sendEmail").toString().equalsIgnoreCase("true")) {
+    public static void sendWarning(String uuid, SchedulerProfile profile) {
+        if(profile.shouldSendEmail()) {
             Sender.sendEmail(uuid);
 
             // This class will send the warnings.
